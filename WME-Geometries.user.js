3d208b7cffef6f52e391f2193b11baff3912db12
Mikhail Veygman <mveygman@gmail.com>
2025-03-09 17:24:38 -0400

    Fix Layer Iteration.

--- WME-Geometries.user.js
+++ WME-Geometries.user.js
@@ -629,7 +629,7 @@ function geometries() {
     }
     // clear all
     function removeGeometryLayers() {
-        for (const l in geometryLayers) {
+        for (const l of geometryLayers) {
             sdk.Map.removeLayer({ layerName: l });
             sdk.LayerSwitcher.removeLayerCheckbox({ name: l });
         }
