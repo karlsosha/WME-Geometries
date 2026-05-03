6f45d749acd905bd3640d263c2a50fde7a168b3e
Mikhail Veygman <mveygman@gmail.com>
2026-05-03 11:05:32 -0400

    Update

--- WME-Geometries.user.js
+++ WME-Geometries.user.js
@@ -8,7 +8,7 @@
 // @match               https://beta.waze.com/*
 // @exclude             https://www.waze.com/*user/*editor/*
 // @require             https://cdn.jsdelivr.net/npm/@tmcw/togeojson@7.1.1/dist/togeojson.umd.min.js
-// @require             https://unpkg.com/@terraformer/wkt
+// @require             https://cdn.jsdelivr.net/npm/@terraformer/wkt@2.2.1/dist/t-wkt.umd.min.js
 // @require             https://cdn.jsdelivr.net/npm/gml2geojson@0.0.7/dist/gml2geojson.min.js
 // @require             https://cdn.jsdelivr.net/npm/@turf/turf@7.3.1/turf.min.js
 // @require             https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,2 @@
 // import WazeWrap from 'https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js';
 declare module 'https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js';
-declare module 'https://cdn.jsdelivr.net/gh/TheEditorX/wme-sdk-plus@1.2.0/wme-sdk-plus.js';
